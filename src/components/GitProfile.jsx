a141a1c4a4eb32ba17cd7fa5773c490191702c06
yoonjiii <109506958+yoonjiii@users.noreply.github.com>
2023-11-24 15:48:14 +0900

    Update GitProfile.jsx

--- src/components/GitProfile.jsx
+++ src/components/GitProfile.jsx
@@ -166,7 +166,7 @@ const GitProfile = ({ config }) => {
                         />
                       )}
                       <AvatarCard
-                        profile={profile}
+                        profile={sanitizedConfig.profile}
                         loading={loading}
                         avatarRing={!sanitizedConfig.themeConfig.hideAvatarRing}
                         resume={sanitizedConfig.resume}
